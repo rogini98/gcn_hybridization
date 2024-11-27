545d0d4b9e7b6f41437f07d51174f763475a8ac8
Rogini Runghen <rogini.runghen@gmail.com>
2024-11-27 18:35:28 -0500

    updated the name of functions that needs to be sourced for script to work

--- introgression/00-master_introgression.R
+++ introgression/00-master_introgression.R
@@ -12,9 +12,9 @@ library(igraph)
 library(Matrix)
 
 # Source required scripts
-source("1-base_functions_introgressionSim.R")
-source("2-run_introgressionSim.R")
-source("3-generate_introgression_plots.R")
+source("./code/introgression/01-functions_introgression.R")
+source("./code/introgression/02-simulation_introgression.R")
+source("./code/introgression/03-visualization_introgression.R")
 
 
 tic()
@@ -22,7 +22,7 @@ tic()
 # Simulation Parameters
 config <- list(
   # Output directory
-  output_dir = "results/introgression_simulation",
+  output_dir = "output/introgression_simulation",
   
   # Basic simulation parameters
   chromosome_lengths = c(10, 10, 5, 5, 10),  # Length of each chromosome
@@ -32,23 +32,23 @@ config <- list(
   
   # Network parameters
   network = list(
-    density00 = 0.02,        # Network density for parent population A
+    density00 = 0.2,         # Network density for parent population A
     effect00 = 2,            # Network effect size for parent population A
-    density11 = 0.03,        # Network density for parent population B
-    effect11 = 1.5           # Network effect size for parent population B
-  ),
+    density11 = 0.1,         # Network density for parent population B
+    effect11 = 2.5           # Network density for parent population B
+  )
   
   # Expression parameters
   expression = list(
     mean = 5,                # Mean expression level
     sd = 2,                  # Standard deviation of expression
-    beta_sd = 3              # Standard deviation of genetic effects
+    beta_sd = 2              # Standard deviation of genetic effects
   ),
   
   # Migration rates
   migration_rates = list(
     # Rates for network analysis (log scale)
-    network = exp(seq(-10, 0, by = 1)),
+    network = exp(seq(-10, 0, by = 0.01)),
     
     # Rates for tracking analysis (specific values)
     tracking = c(0, 0.05, 0.5, 1)
