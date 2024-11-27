94779517f3c57c39b896f2829c16183ecfe3ee29
Rogini Runghen <rogini.runghen@gmail.com>
2024-11-26 22:47:13 -0500

    removed shared functions between both simulations: all network analysis related functions are now in ../shared/network_analysis.R and all core genetics and population related codes are now in the ../shared/helper_functions.R

--- hybridization/01-functions_hybridization.R
+++ hybridization/01-functions_hybridization.R
@@ -2,59 +2,8 @@
 # scenario 1: simulate hybridization
 library(igraph)
 
-#############################################
-### 1. Function to create parents
-makeparents <- function(Chr.lengths) {
-  Chr.names <- paste(rep(paste("Chr", c(1:length(Chr.lengths)), sep = ""), each = 2), c(".1", ".2"), sep = "")
-  
-  ParentA <- sapply(Chr.names, function(x) NULL)
-  ParentB <- sapply(Chr.names, function(x) NULL)
-  for(i in seq(2, length(Chr.names), by = 2)) {
-    ParentA[[i-1]] <- rep(0, Chr.lengths[i/2])
-    ParentA[[i]] <- rep(0, Chr.lengths[i/2])
-    ParentB[[i-1]] <- rep(1, Chr.lengths[i/2])
-    ParentB[[i]] <- rep(1, Chr.lengths[i/2])
-  }
-  return(list(pA = ParentA, pB = ParentB))
-}
 
-#############################################
-### 2. Functions for recombination and mating
-get.gamete <- function(parent) {
-  Chr.names.p <- names(parent)
-  Chr.names <- Chr.names.p[seq(1, length(Chr.names.p)-1, by = 2)]
-  gamete <- sapply(Chr.names, function(x) NULL)
-  
-  for(i in seq(2, length(Chr.names.p), by = 2)) {
-    chromatid1 <- parent[[i-1]]
-    chromatid2 <- parent[[i]]
-    if(runif(1) < 0.5) {
-      chromatid1.1 <- chromatid2
-      chromatid2.1 <- chromatid1
-      chromatid1 <- chromatid1.1
-      chromatid2 <- chromatid2.1
-    }
-    breakpoint <- sample(c(2:(length(chromatid1)-1)), 1)
-    after_crossingover <- c(chromatid1[1:breakpoint], chromatid2[(breakpoint+1):length(chromatid2)])
-    gamete[[i/2]] <- after_crossingover
-  }
-  return(gamete)
-}
 
-#############################################
-### 3. Mating function
-mate <- function(parent1, parent2) {
-  gamete1 <- get.gamete(parent1)
-  gamete2 <- get.gamete(parent2)
-  Chr.names.p <- names(parent1)
-  offspring <- sapply(Chr.names.p, function(x) NULL)
-  
-  for(i in seq(2, length(Chr.names.p), by = 2)) {
-    offspring[[i-1]] <- gamete1[[i/2]]
-    offspring[[i]] <- gamete2[[i/2]]
-  }
-  return(offspring)
-}
 
 #############################################
 ### 4. Population breeding
@@ -97,62 +46,6 @@ genotype_to_nullexpr <- function(genotype, intercepts, betas) {
   return(list(expression = expression, genome_val = genome_val))
 }
 
-#############################################
-### 6. Network architecture
-get_effect.matrix <- function(N_genes, network_density, network_effect, type = "BarabasiAlbert", powerval = 1) {
-  if(type == "binom") {
-    incidences <- rbinom(n = N_genes^2, size = 1, prob = network_density)
-    effects <- rnorm(n = N_genes^2, mean = 0, sd = network_effect)
-    effect.matrix <- matrix(incidences*effects, nrow = N_genes)
-  }
-  if(type == "BarabasiAlbert") {
-    netw <- sample_pa(n=N_genes, powerval, m=1, directed=F)
-    netw.matrix <- as.matrix(as_adj(netw))
-    incidences <- rbinom(n = N_genes^2, size = 1, prob = network_density)
-    effects <- rnorm(n = N_genes^2, mean = 0, sd = network_effect)
-    subsample.matrix <- matrix(incidences*effects, nrow = N_genes)
-    effect.matrix <- incidences*subsample.matrix
-  }
-  return(effect.matrix)
-}
-
-#############################################
-### 7. Network expression
-genotype_to_netexpr <- function(genotype, intercepts, betas, effect.matrices, neteQTLlocus) {
-  placeholder <- genotype_to_nullexpr(genotype, intercepts, betas)
-  genome_val <- placeholder$genome_val
-  null_expression <- placeholder$expression
-  lambda <- exp(intercepts + betas)
-  N_genes <- length(null_expression)
-  effect.matrix <- effect.matrices[[genome_val[neteQTLlocus] + 1]]
-  
-  for(i in 1:(N_genes-1)) {
-    for(j in i:N_genes) {
-      null_expression[j] <- null_expression[j] + effect.matrix[i,j]*(null_expression[i] - lambda[i])
-    }
-  }
-  return(null_expression)
-}
-
-#############################################
-### 8. Population transcriptomes
-getTranscriptomes <- function(Population, N_genes, intercepts, betas, effect.matrices, neteQTLlocus) {
-  PopSize <- length(Population)
-  genotypes <- data.frame(matrix(NA, nrow = PopSize, ncol = N_genes))
-  readcounts <- data.frame(matrix(NA, nrow = PopSize, ncol = N_genes))
-  
-  for(i in 1:PopSize) {
-    ind1 <- Population[[i]]
-    NChr <- length(ind1) 
-    genome_val <- c()
-    for(j in seq(1, NChr-1, by = 2)) {
-      genome_val <- c(genome_val, ind1[[j]] + ind1[[j+1]])
-    }
-    genotypes[i,] <- genome_val
-    readcounts[i,] <- genotype_to_netexpr(ind1, intercepts, betas, effect.matrices, neteQTLlocus)
-  }
-  return(list(g = genotypes, r = readcounts))
-}
 
 #############################################
 ### 9. Network analysis functions
--- introgression/01-functions_introgression.R
+++ introgression/01-functions_introgression.R
@@ -6,68 +6,7 @@
 ### Core Functions for introgression simulation
 #############################################
 
-#' Create parent populations with specified chromosome lengths
-#' @param Chr.lengths Vector specifying length of each chromosome
-#' @return List containing parent populations A and B
-makeparents <- function(Chr.lengths) {
-  Chr.names <- paste(rep(paste("Chr", c(1:length(Chr.lengths)), sep = ""), each = 2), c(".1", ".2"), sep = "")
-  
-  ParentA <- sapply(Chr.names, function(x) NULL)
-  ParentB <- sapply(Chr.names, function(x) NULL)
-  
-  for(i in seq(2, length(Chr.names), by = 2)) {
-    ParentA[[i-1]] <- ParentA[[i]] <- rep(0, Chr.lengths[i/2])
-    ParentB[[i-1]] <- ParentB[[i]] <- rep(1, Chr.lengths[i/2])
-  }
-  
-  return(list(pA = ParentA, pB = ParentB))
-}
-
-#' Generate a gamete through recombination
-#' @param parent Parent genotype
-#' @return Recombined gamete
-get.gamete <- function(parent) {
-  Chr.names.p <- names(parent)
-  Chr.names <- Chr.names.p[seq(1, length(Chr.names.p)-1, by = 2)]
-  gamete <- sapply(Chr.names, function(x) NULL)
-  
-  for(i in seq(2, length(Chr.names.p), by = 2)) {
-    chromatid1 <- parent[[i-1]]
-    chromatid2 <- parent[[i]]
-    
-    if(runif(1) < 0.5) {
-      chromatid1.1 <- chromatid2
-      chromatid2.1 <- chromatid1
-      chromatid1 <- chromatid1.1
-      chromatid2 <- chromatid2.1
-    }
-    
-    breakpoint <- sample(c(2:(length(chromatid1)-1)), 1)
-    after_crossingover <- c(chromatid1[1:breakpoint], chromatid2[(breakpoint+1):length(chromatid2)])
-    gamete[[i/2]] <- after_crossingover
-  }
-  
-  return(gamete)
-}
-
-#' Mate two parents to produce offspring
-#' @param parent1,parent2 Parent genotypes
-#' @return Offspring genotype
-mate <- function(parent1, parent2) {
-  gamete1 <- get.gamete(parent1)
-  gamete2 <- get.gamete(parent2)
-  Chr.names.p <- names(parent1)
-  offspring <- sapply(Chr.names.p, function(x) NULL)
-  
-  for(i in seq(2, length(Chr.names.p), by = 2)) {
-    offspring[[i-1]] <- gamete1[[i/2]]
-    offspring[[i]] <- gamete2[[i/2]]
-  }
-  
-  return(offspring)
-}
-
-#' Calculate allele frequencies
+# 1. Calculate allele frequencies #################
 #' @param population List of individual genotypes
 #' @return Vector of allele frequencies
 get_allele_freq <- function(population) {
@@ -114,110 +53,15 @@ generate_transcriptome_parameters <- function(N_genes, meanexpr = 5, sd.expr = 2
   ))
 }
 
-#' Create gene coexpression network architecture
-#' @param N_genes Number of genes
-#' @param network_density Network density
-#' @param network_effect Network effect size
-#' @return Adjacency matrix
-get_effect_matrix <- function(N_genes, network_density, network_effect) {
-  incidences <- rbinom(n = N_genes^2, size = 1, prob = network_density)
-  effects <- rnorm(n = N_genes^2, mean = 0, sd = network_effect)
-  effect.matrix <- matrix(incidences * effects, nrow = N_genes, ncol = N_genes)
-  return(effect.matrix)
-}
 
-#' Generate transcriptome with network effects
-#' @param genotype Individual genotype
-#' @param intercepts Expression intercepts
-#' @param betas Genetic effect sizes
-#' @param effect_matrices List of network effect matrices
-#' @param neteQTLlocus Network QTL locus
-#' @return Expression values
-genotype_to_netexpr <- function(genotype, intercepts, betas, effect_matrices, neteQTLlocus) {
-  NChr <- length(genotype)
-  genome_val <- c()
-  for(i in seq(1, NChr-1, by = 2)) {
-    genome_val <- c(genome_val, genotype[[i]] + genotype[[i+1]])
-  }
-  
-  lambda <- exp(intercepts + genome_val*betas)
-  null_expression <- rpois(n = length(lambda), lambda)
-  N_genes <- length(null_expression)
-  effect.matrix <- effect_matrices[[genome_val[neteQTLlocus] + 1]]
-  
-  for(i in 1:(N_genes-1)) {
-    for(j in i:N_genes) {
-      null_expression[j] <- null_expression[j] + 
-        effect.matrix[i,j] * (null_expression[i] - lambda[i])
-    }
-  }
-  
-  return(null_expression)
-}
 
-#' Get transcriptomes for a population
-#' @param Population List of individual genotypes
-#' @param N_genes Number of genes
-#' @param intercepts Expression intercepts
-#' @param betas Genetic effect sizes
-#' @param effect_matrices List of network effect matrices
-#' @param neteQTLlocus Network QTL locus
-#' @return List containing genotypes and readcounts
-getTranscriptomes <- function(Population, N_genes, intercepts, betas, effect_matrices, neteQTLlocus) {
-  PopSize <- length(Population)
-  genotypes <- data.frame(matrix(NA, nrow = PopSize, ncol = N_genes))
-  readcounts <- data.frame(matrix(NA, nrow = PopSize, ncol = N_genes))
-  
-  for(i in 1:PopSize) {
-    ind1 <- Population[[i]]
-    NChr <- length(ind1)
-    genome_val <- c()
-    for(j in seq(1, NChr-1, by = 2)) {
-      genome_val <- c(genome_val, ind1[[j]] + ind1[[j+1]])
-    }
-    genotypes[i,] <- genome_val
-    readcounts[i,] <- genotype_to_netexpr(ind1, intercepts, betas, effect_matrices, neteQTLlocus)
-  }
-  
-  return(list(g = genotypes, r = readcounts))
-}
+
+
 
 #############################################
 ### Network Analysis Functions
 #############################################
 
-#' Calculate network metrics for simulation output
-#' @param co_expr Correlation matrix
-#' @param threshold Correlation threshold
-#' @return List of network metrics
-calculate_network_stats <- function(co_expr, threshold = 0.2) {
-  # Calculate adjacency matrix
-  adjacency <- (cor(co_expr) > threshold) * 1
-  diag(adjacency) <- 0
-  
-  # Create igraph object
-  g <- graph_from_adjacency_matrix(adjacency, mode = "undirected")
-  
-  # Calculate metrics
-  degree_vector <- degree(g)
-  mean_degree <- mean(degree_vector)
-  transitivity <- transitivity(g, type = "global")
-  
-  # Calculate modularity
-  comm <- cluster_louvain(g)
-  modularity <- modularity(comm)
-  
-  # Handle NaN values
-  if(is.nan(transitivity)) transitivity <- 0
-  if(is.nan(modularity)) modularity <- 0
-  
-  return(list(
-    mean_degree = mean_degree,
-    transitivity = transitivity,
-    modularity = modularity,
-    graph = g
-  ))
-}
 
 #' Create averaged correlation matrix from multiple replicates
 #' @param results List of simulation results for a given migration rate
